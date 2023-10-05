193b3872a79d41c0f48b3bfdf9a722f2bc0d7689
Ikenna Richard <oguejioforrichard@gmail.com>
2023-10-05 12:24:05 -1100

    Add toggle theme

---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,50 @@
+/* eslint-disable react/prop-types */
+import { createContext } from "react";
+import { useState, useEffect } from "react";
+
+export const ThemeContext = createContext();
+
+const getTheme = () => {
+  const theme = localStorage.getItem("theme");
+  if (!theme) {
+    localStorage.setItem("theme", "dark-theme");
+    return "dark-theme";
+  } else {
+    return theme;
+  }
+};
+
+export const ThemeProvider = ({children}) => {
+
+  const [theme, setTheme] = useState(getTheme);
+
+  const toggleDarkMode = () => {
+    if (theme === "dark-theme") {
+      setTheme("light-theme");
+    } else {
+      setTheme("dark-theme");
+    }
+  }
+
+  useEffect(() => {
+    const refreshTheme = () => {
+      localStorage.setItem("theme", theme);
+    };
+
+    refreshTheme();
+  }, [theme]);
+
+  useEffect(() => {
+    const refreshTheme = () => {
+      localStorage.setItem("theme", theme);
+    };
+
+    refreshTheme();
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme, toggleDarkMode}}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
