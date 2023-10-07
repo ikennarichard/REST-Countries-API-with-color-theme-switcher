ced8ace65ff510a22ce2995433fab9abe5efd494
Ikenna Richard <oguejioforrichard@gmail.com>
2023-10-07 01:17:06 -1100

    Add default options for the query client

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ThemeProvider } from './context/ThemeContext.jsx';
 import App from './App.jsx'
-import './index.css'
+import './css/index.css'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      queryFn: async () => {
+        const response = await fetch(
+          'https://restcountries.com/v3.1/all?fields=name,flags,population,region,capital,continent,subregion,tld,currencies,languages,borders,cca3'
+        );
+        if (!response.ok) {
+          throw new Error('Failed to fetch countries');
+        }
+        return response.json();
+      },
+    }
+  }
+});
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
